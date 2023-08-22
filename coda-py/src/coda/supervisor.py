0a9f315a70c015ac691dfadb3605ff8b7f07eb8f
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-08-22 17:21:47 +0200

    Add tests

--- coda-py/src/coda/supervisor.py
+++ coda-py/src/coda/supervisor.py
@@ -51,51 +51,6 @@ class SupervisorAPI(ABC):
         return self.extract_response(response)
 
 
-class MockSupervisorAPI(SupervisorAPI):
-
-    def __init__(self):
-        self.messages = [
-            {
-                "type": "req",
-                "cmd": "execute_workflow",
-                "args": {
-                    "workflow_name": "MyWorkflow",
-                    "workflow_run_id": uuid.UUID("fea61924-99f5-45f5-82c1-1082efeaa6af").bytes,
-                    "params_id": uuid.UUID("fea61924-99f5-45f5-82c1-1082efeaa6af").bytes,
-                },
-            },
-            {
-                "type": "resp",
-                "request_id": uuid.UUID("fea61924-99f5-45f5-82c1-1082efeaa6af").bytes,
-                "result": 100
-            }
-        ]
-        self.index = 0
-
-    def make_request(self, cmd, args):
-        return SupervisorRequest(
-            cmd=cmd,
-            request_id=uuid.UUID("fea61924-99f5-45f5-82c1-1082efeaa6af").bytes
-        )
-
-    def build_condition_for_response(self, request):
-        return _default_message_condition(
-            _type="resp",
-            request_id=request.request_id
-        )
-
-    def get_next_message(self):
-        if self.index >= len(self.messages):
-            return None
-
-        message = self.messages[self.index]
-        self.index += 1
-        return message
-
-    def extract_response(self, response):
-        return response["result"]
-
-
 class CborSupervisorAPI(SupervisorAPI):
 
     def __init__(self, url):
