f01e84943897abac18fcdf7050f0aa4ac99ca63d
Armin Ronacher <armin.ronacher@active-4.com>
2023-08-23 08:50:59 +0200

    Make console and logging configurable on the command line

--- coda-supervisor/src/cli.rs
+++ coda-supervisor/src/cli.rs
@@ -4,17 +4,21 @@ use std::path::PathBuf;
 use anyhow::Error;
 use clap::{Parser, Subcommand};
 use tracing::metadata::LevelFilter;
-use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::Layer;
 
 use crate::config::Config;
 use crate::controller::Controller;
 
 #[derive(Parser, Debug)]
 pub struct Cli {
+    /// The command to run.
     #[command(subcommand)]
     command: Commands,
+    /// Enable the tracing console
+    #[arg(long, global = true)]
+    tokio_console: bool,
+    /// Sets the log level
+    #[arg(long, global = true, default_value = "warn")]
+    log_level: LevelFilter,
 }
 
 #[derive(Subcommand, Debug)]
@@ -25,14 +29,15 @@ enum Commands {
 
 #[derive(Parser, Debug)]
 pub struct RunCommand {
-    /// The command and it's arguments to execute as worker.
-    args: Vec<OsString>,
     /// The number of workers to spawn.
     #[arg(short = 'n', long = "worker-count")]
     worker_count: Option<usize>,
     /// Path to the config file.
     #[arg(short = 'c', long = "config")]
     config: Option<PathBuf>,
+    /// The command and it's arguments to execute as worker.
+    #[arg(last = true)]
+    args: Vec<OsString>,
 }
 
 async fn run(cmd: RunCommand) -> Result<(), Error> {
@@ -49,12 +54,15 @@ async fn run(cmd: RunCommand) -> Result<(), Error> {
 }
 
 pub async fn execute() -> Result<(), Error> {
-    tracing_subscriber::registry()
-        .with(console_subscriber::spawn())
-        .with(tracing_subscriber::fmt::layer().with_filter(LevelFilter::DEBUG))
-        .init();
+    use tracing_subscriber::prelude::*;
 
     let cli = Cli::parse();
+
+    tracing_subscriber::registry()
+        .with(cli.tokio_console.then(console_subscriber::spawn))
+        .with(tracing_subscriber::fmt::layer().with_filter(cli.log_level))
+        .init();
+
     match cli.command {
         Commands::Run(cmd) => run(cmd).await,
     }
