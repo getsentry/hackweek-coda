58bb41e6bf7a97bcaf32c3f7606c4f2888eeafd0
Riccardo Busetti <riccardob36@gmail.com>
2023-08-24 16:43:20 +0200

    Fix

--- coda-py/src/application/tasks.py
+++ coda-py/src/application/tasks.py
@@ -3,15 +3,17 @@ import coda
 
 @coda.task()
 async def normalize_event(project_id, event_data):
+    event_data["normalize_event"] = True
     return event_data
 
 
 @coda.task()
 async def symbolicate_event(project_id, event_data):
+    event_data["symbolicate_event"] = True
     return event_data
 
 
 @coda.task()
 async def store_event(project_id, event_data):
-    print("EXECUTING STORE")
+    event_data["store_event"] = True
     return event_data
